27f5d299a85867c27c6613cb562c17814ed45492
olumide-oloruntoba <olumide.oloruntoba@detroitlabs.com>
2023-06-08 10:19:03 -0400

    update

--- main.tf
+++ main.tf
@@ -50,3 +50,22 @@ resource "google_project" "main" {
   name       = "My Project"
   project_id = "ninth-victor-389223"
 }
+
+resource "google_storage_bucket" "static-site" {
+  name          = "image-store.com"
+  location      = "EU"
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["http://image-store.com"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+}
